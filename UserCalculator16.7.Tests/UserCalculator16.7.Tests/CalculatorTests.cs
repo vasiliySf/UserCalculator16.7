f4aebfb0a589f262e2ade6f2927d7d1e11e34899
vasiliySf <vasiliy.ts@gmail.com>
2022-06-25 03:57:43 +0300

    1

--- UserCalculator16.7.Tests/UserCalculator16.7.Tests/CalculatorTests.cs
+++ UserCalculator16.7.Tests/UserCalculator16.7.Tests/CalculatorTests.cs
@@ -11,10 +11,34 @@ namespace UserCalculator16._7.Tests
     public class CalculatorTests
     {
         [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(30, 10) == 40);
+        }
+        [Test]
         public void Subtraction_MustReturnCorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(20, 10) == 200);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
         }
     }
 }
